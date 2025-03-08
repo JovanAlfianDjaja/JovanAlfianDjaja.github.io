6e10b4e764e421eba0eea368401053f33d70826e
JovanAlfianDjaja <121505336+JovanAlfianDjaja@users.noreply.github.com>
2025-03-09 00:04:55 +0700

    Update lastUpdate.js

--- lastUpdate.js
+++ lastUpdate.js
@@ -1,3 +1,3 @@
 // document.write("Last Modified: " + moment(document.lastModified).format('Do MMMM YYYY, H:mm:ss'));
-document.write(moment(document.lastModified).format('YYYY'));
+document.write(moment().format('YYYY'));
 
